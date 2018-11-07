0ea636c8145c1224c133b0b074c06c78e8f9390e
Pedro Villamor Lopez <pedro88vl@gmail.com>
2018-11-07 19:46:20 +0000

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create(
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3
+)
+Flat.create(
+  name: 'Flat Hackney',
+  address: 'Kingsland Road',
+  description: 'Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 80,
+  number_of_guests: 4
+)
+Flat.create(
+  name: 'Shitty flat',
+  address: 'Commercial Road',
+  description: 'Shitty flat',
+  price_per_night: 70,
+  number_of_guests: 2
+)
+Flat.create(
+  name: 'Victorian House',
+  address: 'Northchurch Road',
+  description: 'Old but gold',
+  price_per_night: 120,
+  number_of_guests: 4
+)
